f5f59637e9466479a0df24643c564956c977a998
PneumaticPiston <109473877+PneumaticPiston@users.noreply.github.com>
2025-10-08 17:13:26 +0000

    Ugs

--- public/game.js
+++ public/game.js
@@ -155,12 +155,12 @@ class RealOrFakeGame {
         
         const leftImg = document.createElement('img');
         leftImg.className = 'game-image';
-        leftImg.src = `https://github.com/PneumaticPiston/real-or-fake-game/blob/main/images/${images[shuffledPositions[0]].type}/${images[shuffledPositions[0]].file}`;
+        leftImg.src = `https://github.com/PneumaticPiston/real-or-fake-game/blob/main/images/${images[shuffledPositions[0]].type}/${images[shuffledPositions[0]].file}?raw=true`;
         leftImg.dataset.type = images[shuffledPositions[0]].type;
         
         const rightImg = document.createElement('img');
         rightImg.className = 'game-image';
-        rightImg.src = `https://github.com/PneumaticPiston/real-or-fake-game/blob/main/images/${images[shuffledPositions[1]].type}/${images[shuffledPositions[1]].file}`;
+        rightImg.src = `https://github.com/PneumaticPiston/real-or-fake-game/blob/main/images/${images[shuffledPositions[1]].type}/${images[shuffledPositions[1]].file}?raw=true`;
         rightImg.dataset.type = images[shuffledPositions[1]].type;
         
         await Promise.all([this.loadImagePromise(leftImg), this.loadImagePromise(rightImg)]);
